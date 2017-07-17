3ec9964c34a853d0cdb186df23252278c105dab7
Reyes Antequera <rantmed87@gmail.com>
2017-07-17 17:51:19 +0200

    Pelea creada

--- vikings.js
+++ vikings.js
@@ -1,3 +1,9 @@
+
+var healthMinim = 40;
+
+
+
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -15,3 +21,40 @@ var allVikings = [];
 names.forEach(function (name){
     allVikings.push(new Viking(name));
 });
+
+var pos1 = allVikings.indexOf(allVikings[Math.floor(Math.random()*allVikings.length)]);
+var pos2 = allVikings.indexOf(allVikings[Math.floor(Math.random()*allVikings.length)]);
+
+var vikingo1 = allVikings.splice(pos1,1)[0];
+var vikingo2 = allVikings.splice(pos2,1)[0];
+
+
+function turnoPelea(player1,player2){
+	var inicio = Math.floor(Math.random())+1;
+	
+	if (inicio == 1 ){
+		player2.health -= Math.floor(player1.strengh * 0.2 * (randomNumber(0.7, 1.1)));
+		player1.health -= Math.floor(player2.strengh * 0.2 * (randomNumber(0.7, 1.1)));
+	}
+	else {
+		player1.health -= Math.floor(player2.strengh * 0.2 * (randomNumber(0.7, 1.1)));
+		player2.health -= Math.floor(player1.strengh * 0.2 * (randomNumber(0.7, 1.1)));		
+	}
+
+}
+
+
+do {
+	turnoPelea(vikingo1,vikingo2)
+	console.log(vikingo1,vikingo2);
+	if (vikingo1.health < healthMinim){
+	  console.log('El ganador es ' + vikingo2.name);
+	}
+	else if (vikingo2.health < healthMinim  ){
+	  console.log('El ganador es ' + vikingo1.name);
+	}
+	
+	
+
+} while (vikingo1.health >= healthMinim && vikingo2.health >= healthMinim);
+
