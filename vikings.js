4c20dd0ff6098262a0cd0e5492cdf4b3a2810d5c
alvaro <amianalvarez@gmail.com>
2017-07-17 18:53:13 +0200

    Final version

--- vikings.js
+++ vikings.js
@@ -1,8 +1,8 @@
+//Numero aleatorio en un intervalo (min,max)
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-var healthMinim = 40;
-
+//Prototipos 'Vikingo' y 'Sajon'
 var Viking = function(name){
     this.name = name;
     this.health = randomNumber(minViking,maxViking);
@@ -13,22 +13,24 @@ var Saxon = function(name){
     this.health = randomNumber(minSax,maxSax);
     this.strengh = randomNumber(minSax,maxSax);
 };
+//Parametros
 var minViking = 80;
 var maxViking = 120;
 var minSax = 60;
 var maxSax = 100;
+var saxonsArmy = 20;
 var names = ['Astrid','Brynhild','Freydis','Gudrun','Gunnhild','Gunnvor','Hilde','Ingrid','Ragnhild','Ranveig','Sigrid','Sigrunn','Siv','Solveig','Svanhild','Torhild','Torunn','Turid','Vigdis','Yngvild'];
-
+//Creacion de ejercitos
 var allVikings = [];
 names.forEach(function (name){
     allVikings.push(new Viking(name));
 });
 
 var allSaxons = [];
-for (var i=0, l=names.length; i < l; i++){
+for (var i=0, l=saxonsArmy; i < l; i++){
  allSaxons.push(new Saxon('Saxon_'+(i+1)));
 }
-
+//Turno de una pelea entre dos individuos
 function turnoPelea(player1,player2){
 	var inicio = Math.floor(Math.random())+1;
 	if (inicio == 1 ){
@@ -39,7 +41,7 @@ function turnoPelea(player1,player2){
 		player1.health -= Math.floor(player2.strengh * 0.2 * (randomNumber(0.7, 1.1)));
 		player2.health -= Math.floor(player1.strengh * 0.2 * (randomNumber(0.7, 1.1)));
 	}
-}
+} //Pelea completa entre dos individuos
 function aPelear(vikingo, saxon){
     var turnos = 0;
     do {
@@ -53,7 +55,7 @@ function aPelear(vikingo, saxon){
         turnos++;
     } while (vikingo.health >= 0 && saxon.health >= 0 && turnos < 8);
 }
-
+//Batalla completa entre los dos ejercitos
 do {
     var pos1 = allVikings.indexOf(allVikings[Math.floor(Math.random()*allVikings.length)]);
     var pos2 = allSaxons.indexOf(allSaxons[Math.floor(Math.random()*allSaxons.length)]);
@@ -61,77 +63,9 @@ do {
     var saxon = allSaxons.splice(pos2,1)[0];
     aPelear(vikingo, saxon);
     }while(allSaxons.length > 0 && allVikings.length > 0);
-
+//Victoria
 if (allSaxons.length === 0){
     alert('LOS VIKINGOS HAN GANADO!!! '+'\n'+'\n'+' Porcentaje de bajas: '+(1-allVikings.length/20)*100+'%');
 }else{
-    alert('LOS SAJONES HAN GANADO!!! '+'\n'+'\n'+' Porcentaje de bajas: '+(1-allSaxons.length/20)*100+'%');
-}function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-var healthMinim = 40;
-
-var Viking = function(name){
-    this.name = name;
-    this.health = randomNumber(minViking,maxViking);
-    this.strengh = randomNumber(minViking,maxViking);
-};
-var Saxon = function(name){
-    this.name = name;
-    this.health = randomNumber(minSax,maxSax);
-    this.strengh = randomNumber(minSax,maxSax);
-};
-var minViking = 80;
-var maxViking = 120;
-var minSax = 60;
-var maxSax = 100;
-var names = ['Astrid','Brynhild','Freydis','Gudrun','Gunnhild','Gunnvor','Hilde','Ingrid','Ragnhild','Ranveig','Sigrid','Sigrunn','Siv','Solveig','Svanhild','Torhild','Torunn','Turid','Vigdis','Yngvild'];
-
-var allVikings = [];
-names.forEach(function (name){
-    allVikings.push(new Viking(name));
-});
-
-var allSaxons = [];
-for (var i=0, l=names.length; i < l; i++){
- allSaxons.push(new Saxon('Saxon_'+(i+1)));
-}
-
-function turnoPelea(player1,player2){
-	var inicio = Math.floor(Math.random())+1;
-	if (inicio == 1 ){
-		player2.health -= Math.floor(player1.strengh * 0.2 * (randomNumber(0.7, 1.1)));
-		player1.health -= Math.floor(player2.strengh * 0.2 * (randomNumber(0.7, 1.1)));
-	}
-	else {
-		player1.health -= Math.floor(player2.strengh * 0.2 * (randomNumber(0.7, 1.1)));
-		player2.health -= Math.floor(player1.strengh * 0.2 * (randomNumber(0.7, 1.1)));		
-	}
-}
-function aPelear(vikingo, saxon){
-    var turnos = 0;
-    do {
-        turnoPelea(vikingo,saxon);
-        if (vikingo.health < 0){
-            allSaxons.push(saxon);
-        }
-        else if (saxon.health < 0){
-            allVikings.push(vikingo);
-        }
-        turnos++;
-    } while (vikingo.health >= 0 && saxon.health >= 0 && turnos < 8);
-}
-
-do {
-    var pos1 = allVikings.indexOf(allVikings[Math.floor(Math.random()*allVikings.length)]);
-    var pos2 = allSaxons.indexOf(allSaxons[Math.floor(Math.random()*allSaxons.length)]);
-    var vikingo = allVikings.splice(pos1,1)[0];
-    var saxon = allSaxons.splice(pos2,1)[0];
-    aPelear(vikingo, saxon);
-    }while(allSaxons.length > 0 && allVikings.length > 0);
-
-if (allSaxons.length === 0){
-    alert('LOS VIKINGOS HAN GANADO!!! '+'\n'+'\n'+' Porcentaje de bajas: '+(1-allVikings.length/20)*100+'%');
-}else{
-    alert('LOS SAJONES HAN GANADO!!! '+'\n'+'\n'+' Porcentaje de bajas: '+(1-allSaxons.length/20)*100+'%');
+    alert('LOS SAJONES HAN GANADO!!! '+'\n'+'\n'+' Porcentaje de bajas: '+(1-allSaxons.length/saxonsArmy)*100+'%');
 }
